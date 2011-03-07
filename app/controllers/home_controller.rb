fbf8b62c4aa31d76e254ee4615d6f204cde3b5bd
Jayphen <jayphen@gmail.com>
2011-03-07 22:17:37 +1100

    Show folders on home, new folder button in nav:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
+      #load current_user's folders
+      @folders = current_user.folders.order("name desc")
+      
+      #load current_user's files(assets)
       @assets = current_user.assets.order("uploaded_file_file_name desc")
     end
   end
